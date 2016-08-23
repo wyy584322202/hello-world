970b3121982e2f52adbf31738eba01884cb5074a
wyy <584322202@qq.com>
2016-08-23 22:03:47 +0800

    new file hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: wyy
+    > Mail: 584322202@qq.com 
+    > Created Time: Tue 23 Aug 2016 09:59:54 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world" << endl;
+	return 0;
+}
